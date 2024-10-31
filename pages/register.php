96ed0ff961e55188ce97ae5d17bfdbf06eabbda6
guiribajustin2004 <127721330+guiribajustin2004@users.noreply.github.com>
2024-10-31 17:54:45 +0800

    Test

---
+++ pages/register.php
@@ -0,0 +1,21 @@
+<html>
+
+    <head>
+        <title>Register Page</title>
+    </head>
+
+    <body> 
+
+        <form action="pages/login.php" method="POST">
+            <label>Email:</label><br>
+            <input type="text" name="email"><br>
+            <label>Password:</label><br>
+            <input type="password"><br>
+            <label>Confirm Password:</label><br>
+            <input type="password"><br>
+            <input type="submit">
+        </form>
+
+    </body>
+
+</html>
